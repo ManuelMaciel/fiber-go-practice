65fa95b69b3efcf92a30994e2b7cabe44f33c7a4
Jesús Acuña <34004828+ManuelMaciel@users.noreply.github.com>
2022-08-17 17:00:47 -0400

    installed uuid and add post method

--- main.go
+++ main.go
@@ -2,7 +2,34 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
 )
+
+type User struct {
+	Firstname string
+	Lastname  string
+	Id        string
+}
+
+func handleUser(c *fiber.Ctx) error {
+	user := User{
+		Firstname: "John",
+		Lastname:  "Doe",
+	}
+	return c.Status(fiber.StatusOK).JSON(user)
+}
+
+func handleCreateUser(c *fiber.Ctx) error {
+	user := User{}
+
+	if err := c.BodyParser(&user); err != nil {
+		return err
+	}
+
+	user.Id = uuid.New().String()
+
+	return c.Status(fiber.StatusOK).JSON(user)
+}
 
 func main() {
 	app := fiber.New()
@@ -10,6 +37,9 @@ func main() {
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello World!")
 	})
+
+	app.Get("/user", handleUser)
+	app.Post("/user", handleCreateUser)
 
 	app.Listen(":3000")
 }
